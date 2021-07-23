f5408c5d363a2976736e6e66ffb265e78cd1b861
Bishal Suvechha Manindra <suvechhabishal@gmail.com>
2021-07-23 09:24:35 +0530

    first commit

--- euclid-algo-GCD.cpp
+++ euclid-algo-GCD.cpp
@@ -1,10 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// code start
+
 int gcd(int a, int b){
     if(a==0) return b;
     return gcd(b%a, a);
 }
+
+// code end
 
 int main(){
     cout<<gcd(35,10)<<endl;
